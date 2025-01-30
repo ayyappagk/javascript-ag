ff8b7c5031df1f37c2a789c79b29f55114111513
ayyappagk <ayyappagk22@gmail.com>
2025-01-30 22:13:01 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,36 @@
+
+// Notes
+
+/*
+V8 Engine for javscript is hidden in chrome browser
+
+Q.What is the difference in saving the file as .js or .txt?
+A.The syntax for v8 Engine is defined in such a way that when v8 engine encounters .js file:
+ v8 engine will identify the keywords and do respective action.
+   For Ex:
+   console.log     : log the output on to console
+   let, var, const : assign them in the memory etc,.
+   
+*/
+
+// const accountId=3214214      //memory assigned with name of reference variable and value can't be re-assigned.
+// // accountId=2  //not allowed
+// console.log(accountId)
+
+let accountEmail="ag@gmail.com"
+accountEmail="ag@ag.com"
+console.log(accountEmail)
+
+
+
+var accountPassword="12345"
+accountPassword="55555"
+console.log(accountPassword);
+
+
+
+// accountCity="bangalore"
+
+
+
+
