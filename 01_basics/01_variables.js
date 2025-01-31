714574e248a19c84732764816ade9cce89e7b95f
ayyappagk <ayyappagk22@gmail.com>
2025-01-31 13:05:20 +0530

    variables notes added

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -13,23 +13,40 @@ A.The syntax for v8 Engine is defined in such a way that when v8 engine encounte
    
 */
 
-// const accountId=3214214      //memory assigned with name of reference variable and value can't be re-assigned.
-// // accountId=2  //not allowed
-// console.log(accountId)
+/*
+When to Use var, let, or const?
+1. Always declare variables
+
+2. Always use const if the value should not be changed
+
+3. Always use const if the type should not be changed (Arrays and Objects)
+
+4. Only use let if you can't use const
+
+5. Only use var if you MUST support old browsers.
+*/
+
+//For variables Interview Qs:
+//ref:https://www.hellojavascript.info/docs/general-javascript-questions/javascript-fundamentals/variables
+//ref:https://chatgpt.com/c/679c5a75-f164-8007-adb0-f151f77f9ebd
+
+
+const accountId=3214214 //memory assigned with name of reference variable and value can't be re-assigned.
+// accountId=2  // re-assigning not allowed for const variable.
+
 
 let accountEmail="ag@gmail.com"
-accountEmail="ag@ag.com"
-console.log(accountEmail)
-
-
-
 var accountPassword="12345"
-accountPassword="55555"
+let address;
+
+console.log(accountId)
+console.log(accountEmail)
 console.log(accountPassword);
+console.log(address)
+
+console.table({accountId,accountEmail,accountPassword,address})
 
 
-
-// accountCity="bangalore"
 
 
 
